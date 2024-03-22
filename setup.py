86206996bbc63190e804f6ee2d9f87eb9cd6bd2b
Brais Lorenzo <blorenfer@gmail.com>
2024-03-22 19:51:12 +0100

    setup.py configured for development install

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="{{ cookiecutter.project_name }}",
-    version="0.1.0",  
+    name="danelfin_demo",
+    version="0.1.1",
     description="A short description of your project",  # Add a description
     packages=find_packages(),
     install_requires=[  # List of external dependencies
         "pytest==8.1.1",
         "pydantic==1.10.13",
+        "pydantic[email]",
         "fire==0.5.0",
         "loguru==0.7.2",
-    ],
+        "TinyDB",
+        "pyarrow",
+        "pytest",
+        "pandas"
+    ]
 )
