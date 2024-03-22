0545588e007ca52bfb25b385a645b5650c8739ff
Brais Lorenzo <blorenfer@gmail.com>
2024-03-22 17:55:59 +0100

    Logging and dump function revised

--- danelfin_demo/main.py
+++ danelfin_demo/main.py
@@ -9,8 +9,6 @@ import pyarrow.dataset as ds
 import pandas as pd
 from loguru import logger
 
-logger.debug("That's it, beautiful and simple logging!")
-
 db = TinyDB('db.json') # declaring db variable.
 
 class Customer(BaseModel): # Creation of customer class for add_customer
@@ -31,6 +29,7 @@ class CustomerManager: # Customer manager class will handle the methods.
         self.customers.append(customer.dict())
         customer_dict = customer.dict()
         customer_dict['id'] = self.id # adds
+        logger.info(f' add_customer method ran successfully with return: {customer_dict}')
         return customer_dict
 
     def get_id(self): #method that gets the len of the database to get it's last ID, not fool-proof to removal of rows.
@@ -39,11 +38,13 @@ class CustomerManager: # Customer manager class will handle the methods.
             self.id = last_row.doc_id + 1
         else:
             self.id = 1
+        #logger.info(f' get_id method ran successfully with return : {self.id}') -
         return self.id
 
     def get_customer(self, id): ##MISSING error if id doesn't exist in database.
         User = Query()
         customer = self.db.search(User.id == id)
+        logger.info(f' Customer method ran successfully with return: {customer}')
         return(customer)
 
     def dump(self, db): # method for dumping a parquet file
@@ -56,11 +57,14 @@ class CustomerManager: # Customer manager class will handle the methods.
                                ('id', pa.int64())]) # Define the schema for the dump
         partition_scheme = ds.partitioning(pa.schema([pa.field('country', pa.string())]), flavor=None) #Define partitioning object
         parquet_file = pq.write_to_dataset(table =pa_dataset ,root_path=  os.getcwd(), partitioning = partition_scheme, schema=my_schema, basename_template= 'dump{i}.parquet', existing_data_behavior= 'overwrite_or_ignore')
+        logger.info(f' dump method ran successfully with output partitioned in {pd_dataset.country.unique()} folders')
         return parquet_file
 
 def main(): #Main function, with argparse you can input different flags.
-    parser = argparse.ArgumentParser(description="Inserts a customer.")
-    parser.add_argument("method", choices=["add_customer", "get_customer",'dump'], help="Method to run") #
+    logger.add("output.log", rotation="20 MB") #Configuring logger output file
+
+    parser = argparse.ArgumentParser(description="Insert the values that will be used by the corresponding method.")
+    parser.add_argument("method", choices=["add_customer", "get_customer",'dump'], help="Method to run") # Select which method will be run
     parser.add_argument("--name", type=str, help="Name", required=False)
     parser.add_argument("--email", type=str, help="Email", required=False)
     parser.add_argument("--age", type=int, help="Age", required=False)
@@ -68,7 +72,7 @@ def main(): #Main function, with argparse you can input different flags.
     parser.add_argument("--id", type=int, help="Id to query when using get_customer", required=False)
     args = parser.parse_args()
 
-    if args.method == "add_customer":
+    if args.method == "add_customer": #If method selected is add_customer, the following code will be executed
         customer_manager = CustomerManager()
         try:
             customer_info = customer_manager.add_customer(name=args.name, email=args.email, age=args.age,
@@ -76,25 +80,27 @@ def main(): #Main function, with argparse you can input different flags.
             print("Customer added:", customer_info)
             db.insert(customer_info)
             sys.exit('Customer added succesfully')
-        except ValueError as e:
+        except ValueError as e: #add_customer Error display.
             print("Error adding customer:", e)
-    if args.method == "get_customer":
+
+    if args.method == "get_customer":#If method selected is get_customer, the following code will be executed
         customer_manager = CustomerManager()
         try:
             customer_id = customer_manager.get_customer(id= args.id)
             print("Customer retrieved:", customer_id)
             sys.exit('Customer retrieved succesfully') ### To-do: Add option if id doesn't exist in database.
-        except ValueError as e:
+        except ValueError as e:#get_customer Error display.
             print("Error retrieving customer:", e)
-    if args.method == "dump":
+
+    if args.method == "dump":#If method selected is dump, the following code will be executed
         customer_manager = CustomerManager()
         try:
             customer_id = customer_manager.dump(db)
             sys.exit('dumped succesfully')
-        except ValueError as e:
+        except ValueError as e:#dump Error display.
             print("Error dumping:", e)
     else:
-        print("Invalid method specified.")
+        print("Invalid method specified.") #If input doesn't adjust to the method, this will show.
 
 if __name__ == "__main__":
     main()
